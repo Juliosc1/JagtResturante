a12e18344e62814ebc91c57bd9ba87e0c6ad970b
juliosc1 <julio.siklander@gmail.com>
2022-11-21 09:37:38 +0100

    Initial commit

---
+++ src/main/java/com/jagt/jagtresturante/JagtResturanteApplication.java
@@ -0,0 +1,13 @@
+package com.jagt.jagtresturante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagtResturanteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JagtResturanteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jagt/jagtresturante/JagtResturanteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jagt.jagtresturante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagtResturanteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
