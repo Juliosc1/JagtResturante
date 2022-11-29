0977daeb467fe401933879dc4b31eeee91c87590
juliosc1 <julio.siklander@gmail.com>
2022-11-29 10:09:31 +0100

    update httpstatus

--- src/main/java/com/jagt/jagtresturante/exception/ApiExceptionHandler.java
+++ src/main/java/com/jagt/jagtresturante/exception/ApiExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @ControllerAdvice //Allows you to handle exceptions across the whole application
@@ -14,7 +13,7 @@ public class ApiExceptionHandler {
     @ExceptionHandler(value = {ApiRequestException.class}) //Used for handling exceptions in specific handler classes
     public ResponseEntity<Object> handleApiRequestException(ApiRequestException exception) {
         //containing exception details
-        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        HttpStatus badRequest = HttpStatus.NOT_FOUND;
 
         ApiException apiException = new ApiException(
                 exception.getMessage(), badRequest, ZonedDateTime.now()
