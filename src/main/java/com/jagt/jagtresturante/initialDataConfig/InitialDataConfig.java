deb81493ab0cc56c1ebdf12458ac2b37ee97f5b2
Annaaste <anna.dyberg@gmail.com>
2022-11-30 10:51:06 +0100

    Changed name on table and dishes

--- src/main/java/com/jagt/jagtresturante/initialDataConfig/InitialDataConfig.java
+++ src/main/java/com/jagt/jagtresturante/initialDataConfig/InitialDataConfig.java
@@ -14,12 +14,12 @@ public class InitialDataConfig {
     @Bean
     CommandLineRunner commandLineRunner(MenuRepository menuRepository) {
         return args -> {
-            Menu menu = new Menu("Pasta Carbonara","Main Course", "Pasta", 10);
-            Menu menu1 = new Menu("Classic lasagne", "Main Course","Pasta", 12);
-            Menu menu2 = new Menu("Crostini", "Appetizers", "Bread", 5);
-            Menu menu3 = new Menu("Lemon Gelato", "Dessert", "Ice Cream", 7);
-            Menu menu4 = new Menu("Pizza Ai Funghi", "Main Course", "Pizza", 13);
-            menuRepository.saveAll(List.of(menu, menu1, menu2, menu3, menu4));
+            Menu dish1 = new Menu("Pasta Carbonara","Main Course", "Pasta", 10);
+            Menu dish2 = new Menu("Classic lasagne", "Main Course","Pasta", 12);
+            Menu dish3 = new Menu("Crostini", "Appetizers", "Bread", 5);
+            Menu dish4 = new Menu("Lemon Gelato", "Dessert", "Ice Cream", 7);
+            Menu dish5 = new Menu("Pizza Ai Funghi", "Main Course", "Pizza", 13);
+            menuRepository.saveAll(List.of(dish1, dish2, dish3, dish4, dish5));
         };
     }
 }
